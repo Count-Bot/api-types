d63049946b6d072549d68b0768f52a8532add5da
Jaren Goldberg <fyrlex@gmail.com>
2026-05-31 20:37:45 -0400

    feat: add user modifiers field

--- payloads/UserModifiers.ts
+++ payloads/UserModifiers.ts
@@ -1,5 +1,20 @@
 export interface APICountingUserModifiers {
   userId: string;
+  coins: {
+    value: number;
+    default: number;
+    endsAt: string | null;
+  };
+  luck: {
+    value: number;
+    default: number;
+    endsAt: string | null;
+  };
+  xp: {
+    value: number;
+    default: number;
+    endsAt: string | null;
+  };
   freeze: {
     value: boolean;
     endsAt: string | null;
